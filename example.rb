d84c55cfe1533301e87932859ad1fc78280155e3
dkonayuki <dkonayuki@gmail.com>
2016-12-21 10:41:21 +0900

    remove test code

--- example.rb
+++ example.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
-#require 'dash_ruby'
-require_relative './lib/button.rb'
+require 'dash_ruby'
 
 button = Dash::Button.new('08:00:27:53:b1:36', 'eth0')
 button.on_pressed do |packet|
